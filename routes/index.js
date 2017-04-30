11070d4614495e8caf15b0fd3ffd9b5f1a852ed7
Rusman Wahab <hwalyonglance@gmail.com>
2017-04-30 09:57:18 +0700

    heroku commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Rusman Wahab' });
 });
 
 module.exports = router;
