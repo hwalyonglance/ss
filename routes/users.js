b84d6b3fd551d910689739f2ebab99414c5565de
Rusman Wahab <hwalyonglance@gmail.com>
2017-05-01 10:09:27 +0700

    2017-05-01 10:10

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('rwarm-tundra-98883');
-});
-
-module.exports = router;
